1774ce421e36b83efe8970f959186455d65f98e6
ljsnell <ljsnell7@gmail.com>
2024-09-02 11:10:25 -0500

    Added unit test for kilometer function

---
+++ program_1.py
@@ -0,0 +1,26 @@
+# Program #1: Kilometer Converter
+# Write a program that asks the user to enter a distance in kilometers, 
+# then converts that distance to miles.  The conversion formula is as follows:  
+# Miles = kilometers x 0.6214.   
+# The conversion must be done as a function with input and output.
+
+
+def kilometer_conversion(kilometers):    
+    miles = 0.0
+    ######################
+    # WRITE YOUR CODE HERE
+    ######################    
+
+
+    # Return the variable to the calling function
+    return miles
+
+#### This piece of the code has been done for you,
+#### you only need to worry about the actual temp 
+#### conversion logic in the temp_conversion function
+if __name__ == '__main__':
+    # Get User Input
+    print('in main')
+    # Call kilometer_conversion
+    
+    # Display the miles
---
+++ program_2.py
@@ -0,0 +1,10 @@
+# Program #2: Math Quiz
+# Write a program that gives simple math quizzes.  The program should display two random numbers to be added, such as
+
+#     247
+
+# + 129
+
+# ------
+
+# The program should allow the student to enter the answer.  If the answer is correct, a message of congratulations should be displayed.  If the answer is incorrect a message showing the correct answer should be displayed.  The program must use a function that accomplishes part of the needed tasks.
