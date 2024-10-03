f57dc4d14074b103b7220fbfcebe639bc5079fea
ljsnell <ljsnell7@gmail.com>
2024-10-02 21:17:46 -0500

    Updated unit test

--- program_1.py
+++ program_1.py
@@ -16,11 +16,11 @@ def kilometer_conversion(kilometers):
     return miles
 
 #### This piece of the code has been done for you,
-#### you only need to worry about the actual temp 
-#### conversion logic in the temp_conversion function
+#### you only need to worry about the actual kilometer
+#### conversion logic in the kilometer_conversion function
 if __name__ == '__main__':
     # Get User Input
     print('in main')
-    # Call kilometer_conversion
+    # Call kilometer_conversion, don't forget to pass in the kilometer parameter!
     
     # Display the miles
